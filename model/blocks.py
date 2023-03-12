033020f3f44b84498c7f037b038e67f91cc4bd1b
jyaacoub <jyaacoub21@gmail.com>
2023-03-11 19:45:49 -0500

    Created transformer blocks
    Next task is to put it all together for the ViT base

--- model/blocks.py
+++ model/blocks.py
@@ -7,29 +7,25 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 class Head(nn.Module):
-  """One head of self-attention"""
-  def __init__(self, n_embd, head_size=32, block_size=128, dropout=0.1, encoder=True):
+  """
+  Encoder head of self-attention (aka non-masked attention)
+  """
+  def __init__(self, n_embd, head_size=32, dropout=0.1):
     super().__init__()
     self.key = nn.Linear(n_embd, head_size, bias=False)
     self.query = nn.Linear(n_embd, head_size, bias=False)
-    self.value = nn.Linear(n_embd, head_size, bias=False)
-    
-    # mask buffer is not a parameter of the model, but is still saved with the model
-    if encoder: # attend to all positions in the sequence
-      self.register_buffer('mask', torch.ones(block_size, block_size))
-    else: # attend to all positions before the current one (decoder)
-      self.register_buffer('mask', torch.tril(torch.ones(block_size, block_size))) # lower triangular matrix
-    
+    self.value = nn.Linear(n_embd, head_size, bias=False)    
     self.dropout = nn.Dropout(dropout)
         
   def forward(self, x):
-    B,T,C = x.shape
+    B,T,C = x.shape   # batch, time, channels
     k = self.key(x)   # what we have
     q = self.query(x) # what we want
     
     # compute attention scores ("affinities")
-    wei = q @ k.transpose(-2, -1) * C**-0.5 # --> (B, T, T), dividing by sqrt(C) is a normalization trick
-    wei = wei.masked_fill(self.mask[:T,:T]==0, float('-inf')) # masking the upper triangle
+    wei = q @ k.transpose(-2, -1) * C**-0.5 # --> (B, T, T), dividing by sqrt(C) is a normalization trick 
+    # so that there is no sharping effect caused by SoftMax where if the values we are running through 
+    # the SoftMax come from a larger domain they will converge to a one-hot vector
     wei = F.softmax(wei, dim=-1)
     wei = self.dropout(wei)
     
@@ -40,52 +36,56 @@ class Head(nn.Module):
   
 class MultiHeadAttention(nn.Module):
   """ Multiple Heads of self-attention in parallel """
-  
-  def __init__(self, num_heads, head_size, n_embd, dropout=0.1):
+  def __init__(self, num_heads, head_size, embd_dim, dropout=0.1):
     super().__init__()
-    # nn.ModuleList is a special container that registers the modules in the list
-    #   "ModuleList can be indexed like a regular Python list, but modules it contains 
-    #   are properly registered, and will be visible by all Module methods." - PyTorch docs
     self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
-    self.proj = nn.Linear(num_heads * head_size, n_embd) # to combine the heads smartly
+    # combine heads by projecting to embedding dimension 
+    self.proj = nn.Linear(num_heads * head_size, embd_dim)
     self.dropout = nn.Dropout(dropout)
     
   def forward(self, x):
-    # run each head in parallel then sum the results
-    out = torch.cat([h(x) for h in self.heads], dim=-1)
-    out = self.proj(out)
+    # run each head in parallel then concatenate the results
+    out = torch.cat([h(x) for h in self.heads], dim=-1) # H,W,C --> H,W,C*num_heads
+    out = self.proj(out) # H,W,C*num_heads --> H,W,C
     out = self.dropout(out)
     return out
-  
-class FeedForward(nn.Module):
-  """ a simple linear layer followed by non-linearity"""  
-  def __init__(self, n_embd=32, dropout=0.1):
+
+class MlpBlock(nn.Module):
+  """MLP block with GELU"""
+  def __init__(self, D, emd_D, dropout=0.1):
     super().__init__()
     self.net = nn.Sequential(
-      nn.Linear(n_embd, 4*n_embd),
-      nn.ReLU(),
-      nn.Linear(4*n_embd, n_embd), # projection layer that maps back to the original dimension
+      nn.Linear(D, emd_D),
+      nn.GELU(),
+      nn.Dropout(dropout),
+      nn.Linear(emd_D, D),
       nn.Dropout(dropout)
     )
     
   def forward(self, x):
     return self.net(x)
 
-class DecoderBlock(nn.Module):
-  """Transformer block: communication via self-attention followed by computation via feed-forward"""
-  
-  def __init__(self, n_embd, n_head) -> None:
+class EncoderBlock(nn.Module):
+  """Single ViT encoder block"""
+  def __init__(self, n_embd, n_head, dropout=0.1) -> None:
     super().__init__()
     head_size = n_embd // n_head
+    self.ln1 = nn.LayerNorm(n_embd, eps=1e-6) #eps is a small value added to the denominator to improve numerical stability
+    
     self.sa = MultiHeadAttention(n_head, head_size)
-    self.ffwd = FeedForward(n_embd)
+    self.ln2 = nn.LayerNorm(n_embd, eps=1e-6)
     
-    self.ln1 = nn.LayerNorm(n_embd)
-    self.ln2 = nn.LayerNorm(n_embd)
+    self.mlp = MlpBlock(n_embd, dropout=dropout)
     
-  def forward(self, x):
-    # layer norm done BEFORE the transformation (prenorm formulation) 
-    # --> this is a deviation from the original paper which did it postnorm
-    x = x + self.sa(self.ln1(x))  # x + is the residual connection
-    x = x + self.ffwd(self.ln2(x))
+    self.dropout = nn.Dropout(dropout)
+    
+  def forward(self, inputs):
+    # layer norm done BEFORE the transformation (prenorm formulation)
+    x = self.ln1(inputs)
+    x = self.sa(x)
+    x = self.dropout(x)
+    x = x + inputs # residual connection
+    
+    # mlp block
+    x = self.mlp(self.ln2(x)) + x
     return x
--- model/model.py
+++ model/model.py
@@ -3,14 +3,22 @@ from config import DATA_PATH_FN, DATA_PATH, LABEL_NAMES
 
 import matplotlib.pyplot as plt
 
-imgs, labels = get_data(DATA_PATH_FN(1)) # get the first batch
-
-for i in range(10):
-    plt.subplot(2, 5, i+1)
-    plt.imshow(imgs[i])
-    plt.title(LABEL_NAMES[labels[i]])
+# class ViT_base:
+#     """Vision Transformer base """
     
-    ax = plt.gca()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-plt.show()
+
+
+
+
+if __name__ == "__main__":
+    imgs, labels = get_data(DATA_PATH_FN(1)) # get the first batch
+
+    for i in range(10):
+        plt.subplot(2, 5, i+1)
+        plt.imshow(imgs[i])
+        plt.title(LABEL_NAMES[labels[i]])
+        
+        ax = plt.gca()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    plt.show()
